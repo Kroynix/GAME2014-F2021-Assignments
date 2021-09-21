f639d9b462bb9a60ec28496a104370f15a356f0b
Nathan Nguyen <zhectx@gmail.com>
2021-09-21 02:38:00 -0400

    Assets Added
    - Added assets that will be later used
    - Created scripts for basic player and missile movement

---
+++ GAME2014-F2021-Assignment/Assets/Scripts/m_PlayerBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class m_PlayerBehaviour : MonoBehaviour
+{    
+    private Vector2 moveInput;
+
+    [SerializeField]
+    public float moveSpeed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPosition = new Vector2(transform.position.x,  transform.position.y);
+
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(inputX,inputY,0) * moveSpeed * Time.deltaTime; 
+    }
+}
